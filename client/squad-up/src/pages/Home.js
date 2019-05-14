4d9b95e799bdc28477ae8ddd13925942b81d06b7
Jalen Okutsu <jalenokutsu@gmail.com>
2019-05-14 13:46:15 -0700

    server file

---
+++ client/squad-up/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class Home extends React.Component {
+
+    rednder() {
+        return (
+            <div>
+                <h1>Home Page</h1>
+            </div>
+        )
+    }
+}
+
+export default Home; 
