8985022d6a135cb2948f1e87e717017619ba4909
Calvin Fronda <cfronda209@gmail.com>
2019-05-23 11:10:55 -0700

    test

--- server/controllers/member/getOne.js
+++ server/controllers/member/getOne.js
@@ -1,6 +1,7 @@
 const { Member } = require('../../models');
 
 module.exports = async ({ params: { name } }, res) => {
+<<<<<<< Updated upstream
     try {
 
         let member = await Member.findOne({name: name});
@@ -9,6 +10,19 @@ module.exports = async ({ params: { name } }, res) => {
     } catch (err) {
         if (err) {
             res.send(err);
+=======
+    console.log(name);
+    if (name) {
+        try {
+            let member = await Member.findOne({ name: name });
+            res.send(member);
+        } catch (err) {
+            if (err) {
+                res.send(err);
+            }
+>>>>>>> Stashed changes
         }
+    } else {
+        res.send('please provide a name');
     }
 }
